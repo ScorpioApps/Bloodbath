3109a4bd8b5ebde748a80159cf5f5dcf7e1d1fb8
Mahi Ahmed <mahiabirahmed@icloud.com>
2018-02-25 21:17:50 +0000

    adding story
    beginning the new code for an RPG game with a proper storyline

--- Bloodbath Story.py
+++ Bloodbath Story.py
@@ -10,4 +10,21 @@ def startUp ():
   print ("- Mage's Apprentice: A young, energetic apprentice on a quest to be a Mage, guided by his wise teacher.")
   print ("- Assassin: An assassin-in-training, taught by a retired soldier to hunt down the countries worst threats.")
   print ("- Warlord: A warrior being taught by a leader in the Bloodbath War.")
+  classChoose ()
+
+def classChoose ():
+  global charClass
+  userInp = input ("> ")
+  if userInp == "Mage's Apprentice":
+    charClass = "Mage's Apprentice"
+    gameStory ()
+  elif userInp == "Assassin":
+    charClass = "Assassin"
+    gameStory ()
+  elif userInp == "Warlord":
+    charClass = "Warlord"
+    gameStory ()
+    
+def gameStory ():
+  if charClass == "Mage's Apprentice":
   
